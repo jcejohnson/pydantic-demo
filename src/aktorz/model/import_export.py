6c1839a090dac237cf5104f834ebd46fc3ebabe6
jcej <jcejohnson@gmail.com>
2021-09-18 11:37:10 -0400

    Refactoring Loader to be more extensible (thinking ahead to the 0.1.x/0.2.0 boundary)

--- src/aktorz/model/import_export.py
+++ src/aktorz/model/import_export.py
@@ -4,8 +4,9 @@ Import (load) and Export (save) functionality.
 
 
 import importlib
+import json
 from enum import Enum, auto
-from pathlib import PosixPath
+from pathlib import PosixPath, Path
 from types import ModuleType as BaseModuleType
 from typing import Optional, Union, cast
 
@@ -52,6 +53,7 @@ class ImportExport:
     @validator("version")
     @classmethod
     def validate_version(cls, v) -> SchemaVersion:
+        # Adding @dataclass to Loader will cause this validator to be ignored. Why?
         if isinstance(v, str):
             return SchemaVersion.create(v)
         return v
@@ -96,9 +98,13 @@ class ImportExport:
         return getattr(cast(ModuleType, values["module"]), "Model")
 
 
-@dataclass
+LoaderType = None
+
+
 class Loader(ImportExport):
     """Load data and create Models."""
+
+    # vvv goes away
 
     def export(self, *args, **kwargs) -> BaseExporter:
         """
@@ -135,6 +141,37 @@ class Loader(ImportExport):
     def has_exporter(self):
         return hasattr(self.module, "Exporter")
 
+    # ^^^^ goes away
+
+    def model(self):
+        """
+        Get the Model class which implements the schema version provided to Loader().
+
+        loader = Loader(...)
+        Model = loader.model()
+        model = Model(**{...})
+        """
+        return getattr(self.module, "Model")
+
+    def loader(self):
+        """
+        Get the Loader class specific to the schema version provided to Loader().
+        If the implementation module does not provide a Loader, return self.
+
+        Used by load() before it delegates to load_input(). You generally do not
+        need to invoke loader() directly.
+        """
+
+        if hasattr(self.module, 'Loader'):
+            loader = cast(ImportExport, getattr(self.module, 'Loader'))
+            return loader(
+                version=self.version,
+                model=self.model,
+                module=self.module
+            )
+
+        return self
+
     @validate_arguments
     def load(
         self,
@@ -145,6 +182,15 @@ class Loader(ImportExport):
     ) -> BaseVersionedModel:
         """
         Create a Model instance from the input data.
+        Delegates to load_input() after fetching the version implementations custom
+        Loader if there is one.
+
+        These are equivalent regardless if there is or is not a custom loader:
+            data = Loader(...).load(...)
+            data = Loader(...).loader().load(...)
+
+        To ignore any potential custom Loader (not recommended) you can do:
+            data = Loader(...).load_input(...)
 
         When `input` is a dict, load() is similar to:
             loader = Loader(...)
@@ -155,115 +201,124 @@ class Loader(ImportExport):
 
         TODO: Document this properly.
 
-        if validate_version is not LoaderValidations.NONE
-            if data.schema_version is not valid against self.version
-                raise ValueError
-            if update_version is True
-                data.schema_version = self.version
-            else
-                data.schema_version = SchemaVersion(data.schema_version)
-
         """
-        return self._load(input=input, validate_version=validate_version, update_version=update_version)
+        do_load = self.load_input if (type(self) == LoaderType) else self.loader().load_input
+        return do_load(input=input, validate_version=validate_version, update_version=update_version)
 
-    def model(self):
-        """
-        Get the Model class which implements the schema version provided to Loader().
-        See also: has_exporter()
-
-        loader = Loader(...)
-        Model = loader.model()
-        model = Model(**{...})
-        """
-        return getattr(self.module, "Model")
-
-    # #### Implementation details
-
-    def _load(
+    def load_input(
         self,
         *,
         input: Union[str, dict, FilePath],
         validate_version: Optional[LoaderValidations] = LoaderValidations.READABLE,
         update_version: Optional[bool] = True,
     ) -> BaseVersionedModel:
+        """
+        Load input data and return a Model.
 
-        # FIXME: Simple, assumptive implementation with 100% coverage.
-        input_type = type(input)
+        Optionally updates `schema_version` in the returned Model to match self.version.
+        """
 
-        assert input_type in [FilePath, PosixPath, dict, str]
+        # Convert the input to a dict
+        raw_data = self._load_raw_data(input)
 
-        if input_type in [FilePath, PosixPath]:
-            data = cast(BaseModel, self.model).parse_file(input)  # type: ignore
-        elif input_type is dict:
-            data = cast(BaseModel, self.model).parse_obj(input)
-        elif input_type is str:
-            data = cast(BaseModel, self.model).parse_raw(input)  # type: ignore
-        else:
-            # TODO: Create a test case for this.
-            raise TypeError(f"Unexpected input type {input.__class__}")
-
-        data = cast(BaseVersionedModel, data)
-        if self._validate_version(
-            data=data,
-            validate_version=cast(LoaderValidations, validate_version),
-            update_version=cast(bool, update_version),
+        # Validate the input data against the implementation model's VERSION
+        if not self.validate_version(
+            input_version=SchemaVersion(raw_data['schema_version']),
+            validate_version=cast(LoaderValidations, validate_version)
         ):
-            return self._make_compatible(data)
+            raise ValueError(
+                f"Input schema version [{data.schema_version}] "
+                f"does not satisfy loader validation [{validate_version}]."
+            )
 
-        raise ValueError(f"Input schema version [{data.schema_version}] is invalid.")
+        # Delegate to pydantic to create a Model from the raw data.
+        data = cast(BaseModel, self.model).parse_obj(raw_data)
 
-    def _make_compatible(self, data: BaseVersionedModel) -> BaseVersionedModel:
-        # Prior to version 0.2.x the Model's schema_version was a str
-        # Dealing with this fundamental and significant type change is one of the
-        # things we're figuring out how to do.
+        if update_version:
+            data.schema_version = self.version
+
+        return self.make_compatible(data)
+
+    def make_compatible(self, data: BaseVersionedModel) -> BaseVersionedModel:
+        """
+        Make `data` compatible with self.version.
+        This may return data, update data inplace or return a new BaseVersionedModel
+        instance.
+
+        Note that this does not (and subclasses should not) update the value of
+        data.schema_version. That will have been done by load_input() before it
+        invokes make_compatible().
+        """
 
         schema_version = data.schema_version  # type: ignore
         if not isinstance(schema_version, SchemaVersion):
-            data = SchemaVersion.create(schema_version)
+            schema_version = SchemaVersion.create(schema_version)
 
+        # Prior to version 0.2.x the Model's schema_version was a str
+        # Dealing with this fundamental and significant type change is one of the
+        # things we're figuring out how to do.
         if schema_version >= SchemaVersion(prefix=schema_version.prefix, semver="0.2.0"):
             return data
 
         data.schema_version = str(schema_version)  # type: ignore
         return data
 
-    def _validate_version(
-        self, *, data: BaseVersionedModel, validate_version: LoaderValidations, update_version: bool
-    ) -> bool:
+    def validate_version(self, *, input_version: SchemaVersion, validate_version: LoaderValidations) -> bool:
         """
-        if validate_version == LoaderValidations.NONE and update_version
-            data.schema_version = self.version (which is a SchemaVersion)
+        Verify that the input_version vs self.module's version satisfies `validate_version`
         """
 
-        # Note: validate_version() ensures that self.version is always a SchemaVersion()
-        assert isinstance(self.version, SchemaVersion)
         assert isinstance(validate_version, LoaderValidations)
+
+        module_version = SchemaVersion.create(self.module.VERSION)
 
         if validate_version == LoaderValidations.NONE:
             return True
 
-        schema_version = data.schema_version  # type: ignore
-
-        data_version = (
-            schema_version if isinstance(schema_version, SchemaVersion) else SchemaVersion.create(schema_version)
-        )
-
         if validate_version == LoaderValidations.IDENTICAL:
-            if self.version != data_version:
-                raise ValueError(f"Input of version [{data_version}] does not match Model version [{self.version}].")
+            if module_version != input_version:
+                raise ValueError(
+                    f"Input of version [{input_version}] does not match "
+                    f"Model version [{module_version}]."
+                )
 
         if validate_version == LoaderValidations.READABLE:
-            if not self.version.can_read(data_version):
+            if not module_version.can_read(input_version):
                 raise ValueError(
-                    f"Input of version [{data_version}] cannot be read by Model version [{self.version}]."
+                    f"Input of version [{input_version}] cannot be read by "
+                    f"Model version [{module_version}]."
                 )
 
         if validate_version == LoaderValidations.WRITABLE:
-            if not self.version.can_write(data_version):
+            if not module_version.can_write(input_version):
                 raise ValueError(
-                    f"Input of version [{data_version}] cannot be written by Model version [{self.version}]."
+                    f"Input of version [{input_version}] cannot be written by "
+                    f"Model version [{module_version}]."
                 )
 
-        data.schema_version = self.version if update_version else data_version  # type: ignore
-
         return True
+
+    # #### Implementation details
+
+    def _load_raw_data(self, input):
+        input_type = type(input)
+
+        assert input_type in [FilePath, PosixPath, dict, str]
+
+        if input_type in [FilePath, PosixPath]:
+            path = cast(Path, input)
+            with open(path.as_posix()) as f:
+                return json.load(f)
+
+        if input_type is dict:
+            return cast(dict, input)
+
+        if input_type is str:
+            return json.loads(cast(str, input))
+
+        # TODO: Create a test case for this.
+        raise TypeError(f"Unexpected input type {input.__class__}")
+
+
+# Update `LoaderType` to refer to our new Loader class
+LoaderType = Loader
