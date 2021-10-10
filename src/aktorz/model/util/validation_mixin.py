c8172b1cb1cfc1af7f35a72e86ddd3c2eb620bba
jcej <jcejohnson@gmail.com>
2021-10-10 15:20:49 -0400

    ValidationMixin cannot change the signature of validate()

--- src/aktorz/model/util/validation_mixin.py
+++ src/aktorz/model/util/validation_mixin.py
@@ -1,18 +1,32 @@
+from typing import TYPE_CHECKING, Any, Type, TypeVar, Union, cast
 
-from typing import Any, Type
+from pydantic import BaseModel
+
+if TYPE_CHECKING:
+    Model = TypeVar("Model", bound="BaseModel")
 
 
 class ValidationMixin:
-    '''
+    """
     MyModel.validate(myModel) doesn't work the way that _I_ need.
     However, MyModel.validate(myModel.dict()) gets the job done.
     This mixin provides that functionality.
 
     See tests/test_validation.py for more.
-    '''
+    """
 
     @classmethod
-    def validate(cls: Type['Model'], value: Any, **dict_kwargs) -> 'Model':
+    def validate(cls: Type["Model"], value: Any) -> BaseModel:
         if isinstance(value, cls):
-            return cls.validate(value.dict(**dict_kwargs))
+            # Create, validate and return a new instance from the dict
+            # representation of `value`.
+            # raises ValidationError if not.
+            new_instance = cls(**value.dict())
+            return super().validate(new_instance)
         return super().validate(value)
+
+    @classmethod
+    def validate_self(cls: Type["Model"], value: BaseModel, **dict_kwargs) -> BaseModel:
+        assert isinstance(value, cls)
+        new_instance = cls(**value.dict(**dict_kwargs))
+        return super().validate(new_instance)
