c4f5132d7ee95bde2f41da1d568dbaf5fde9f5bd
James Johnson <James.Johnson@equifax.com>
2021-10-17 15:16:37 -0400

    Make ValidationMixin available

--- src/aktorz/model/util/__init__.py
+++ src/aktorz/model/util/__init__.py
@@ -3,6 +3,7 @@ from .base_versioned_model import BaseVersionedModel
 from .import_export import Exporter, Loader, LoaderValidations
 from .mixin_commentable_model import CommentableModelMixin
 from .mixin_dict_like import DictLikeMixin
+from .mixin_validate_self import ValidationMixin
 from .mixin_versioned_model import VersionedModelMixin
 from .schema_version import DEFAULT_PREFIX as DEFAULT_VERSION_PREFIX
 from .schema_version import VERSION_REGEX as SCHEMA_VERSION_REGEX
