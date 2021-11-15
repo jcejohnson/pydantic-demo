d1984a4fa2e7cf1d90ca9cc8459929202b6300a0
James Johnson <jcejohnson@gmail.com>
2021-11-14 21:58:30 -0500

    Bump version: 0.2.0-rc1 → 0.2.0-rc2

--- src/aktorz/model/supported_versions.py
+++ src/aktorz/model/supported_versions.py
@@ -8,8 +8,10 @@ SUPPORTED_VERSIONS = [
     # 2021-09-26 18:30:06.899116
     "v0.1.3",
     # 2021-10-30 20:49:39.892464
-    # Current version
-    "v0.2.0-rc1",
+    'v0.2.0-rc1',
+# 2021-11-14 21:58:30.836730
+# Current version
+    "v0.2.0-rc2",
 ]
 
 PREVIOUS_VERSION = SUPPORTED_VERSIONS[-2]
